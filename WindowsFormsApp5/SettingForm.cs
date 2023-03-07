69b9b26321a006d758eb05e1f5b3fe92e3d2336f
RishabhHarman04 <er.23rishabh@gmail.com>
2023-03-07 15:14:41 +0530

    All files

---
+++ WindowsFormsApp5/SettingForm.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp5
+{
+    public partial class SettingForm : Form
+    {
+        public WeatherSettings Settings;
+
+        public SettingForm()
+        {
+            InitializeComponent();
+
+            InitializeFormSize();
+
+            Settings = new WeatherSettings();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (!ValidateCitySelection())
+            {
+                return;
+            }
+
+            GetSettings();
+
+            ShowWeatherForm(Settings);
+
+            this.Close();
+        }
+
+        private WeatherSettings GetSettings()
+        {
+            Settings.Cities = new List<string>();
+            foreach (var item in checkedListBoxCities.CheckedItems)
+            {
+                Settings.Cities.Add(item.ToString());
+            }
+
+            Settings.RefreshTime = numericUpDownTime.Value.ToString();
+            return Settings;
+        }
+
+        private bool ValidateCitySelection()
+        {
+            if (checkedListBoxCities.CheckedItems.Count == 0)
+            {
+                MessageBox.Show(MyStrings.ValidationMessage);
+                return false;
+            }
+
+            return true;
+        }
+
+        private void ShowWeatherForm(WeatherSettings settings)
+        {
+            WeatherData weatherDataForm = Application.OpenForms.OfType<WeatherData>().FirstOrDefault();
+
+            if (weatherDataForm != null)
+            {
+                weatherDataForm.Show();
+            }
+            else
+            {
+                WeatherData weather = new WeatherData(settings);
+
+                weather.Show();
+            }
+        }
+
+        private void InitializeCities()
+        {
+            checkedListBoxCities.Items.Add(MyStrings.City1);
+            checkedListBoxCities.Items.Add(MyStrings.City2);
+            checkedListBoxCities.Items.Add(MyStrings.City3);
+            checkedListBoxCities.Items.Add(MyStrings.City4);
+            checkedListBoxCities.Items.Add(MyStrings.City5);
+            checkedListBoxCities.CheckOnClick = true;
+        }
+
+        private void InitializeRefreshTime()
+        {
+            numericUpDownTime.Minimum = 5;
+            numericUpDownTime.Maximum = 15;
+            numericUpDownTime.Value = 5;
+            numericUpDownTime.Increment = 5;
+        }
+
+        private void SettingForm_Load_1(object sender, EventArgs e)
+        {
+            InitializeCities();
+
+            InitializeRefreshTime();
+        }
+
+        private void InitializeFormSize()
+        {
+            this.Width = 460;
+            this.Height = 400;
+            this.Left = (Screen.PrimaryScreen.Bounds.Width - this.Width) / 2;
+            this.Top = (Screen.PrimaryScreen.Bounds.Height - this.Height) / 2;
+        }
+    }
+}
+
+
+
+
+
---
+++ WindowsFormsApp5/WeatherData.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp5.Properties;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolTip;
+using static WindowsFormsApp5.SettingForm;
+
+
+namespace WindowsFormsApp5
+{
+    public partial class WeatherData : Form
+    {
+        private WeatherService weatherService;
+        private int panelIndex = 0;
+        private int totalCities = 0;
+        private WeatherSettings settings;
+
+        public WeatherData(WeatherSettings settings)
+        {
+            InitializeComponent();
+            InitializeFormSize();
+            this.settings = settings;
+            this.weatherService = new WeatherService(settings);
+
+            if (settings != null && settings.Cities != null && settings.RefreshTime != null)
+            {
+                totalCities = settings.Cities.Count;
+                timer1.Interval = Int32.Parse(settings.RefreshTime) * 1000;
+            }
+
+            RefreshWeatherInfo();
+
+            timer1.Start();
+        }
+
+        private async void timer1_Tick(object sender, EventArgs e)
+        {
+            panelIndex++;
+            if (panelIndex >= totalCities)
+            {
+                panelIndex = 0;
+            }
+
+            RefreshWeatherInfo();
+        }
+
+        private async void RefreshWeatherInfo()
+        {
+            if (settings != null && settings.Cities != null && settings.Cities.Any())
+            {
+                var city = settings.Cities.ElementAt(panelIndex);
+                var weatherData = await weatherService.GetWeatherData(city);
+
+                var weatherInfo = ExtractWeatherData(weatherData);
+
+                UpdateLabels(city, weatherInfo.temperature, weatherInfo.humidity, weatherInfo.pressure);
+            }
+        }
+
+        private WeatherInfo ExtractWeatherData(string weatherData)
+        {
+            dynamic json = Newtonsoft.Json.JsonConvert.DeserializeObject(weatherData);
+            double temperature = json.main.temp;
+            double humidity = json.main.humidity;
+            double pressure = json.main.pressure;
+            return new WeatherInfo(temperature, humidity, pressure);
+        }
+
+        private void UpdateLabels(string city, double temperature, double humidity, double pressure)
+        {
+            label1.Text = city;
+            lblHumidity.Text = String.Format(MyStrings.HumidityText, humidity);
+            lblAtmosphere.Text = String.Format(MyStrings.AtmoshphereText, pressure);
+            lblTemperature.Text = String.Format(MyStrings.TemperatureText, temperature);
+        }
+
+        private async void DisplaySettingForm()
+        {
+            var settingsForm = new SettingForm();
+
+            settingsForm.ShowDialog();
+
+            if (settingsForm.Settings != null)
+            {
+                weatherService = new WeatherService(settingsForm.Settings);
+                settings = settingsForm.Settings;
+
+                if (settings.Cities != null)
+                {
+                    totalCities = settings.Cities.Count;
+                }
+
+                timer1.Interval = Int32.Parse(settings.RefreshTime) * 1000;
+                await Task.Delay(2000);
+
+                RefreshWeatherInfo();
+
+                timer1.Start();
+            }
+        }
+
+        private void btnSettings_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var newSettingsForm = new SettingForm();
+                newSettingsForm.ShowDialog();
+
+                if (newSettingsForm.Settings != null)
+                {
+                    weatherService = new WeatherService(newSettingsForm.Settings);
+                    settings = newSettingsForm.Settings;
+
+                    if (settings.Cities != null)
+                    {
+                        totalCities = settings.Cities.Count;
+                    }
+
+                    timer1.Interval = Int32.Parse(newSettingsForm.Settings.RefreshTime) * 1000;
+
+                    RefreshWeatherInfo();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(String.Format(MyStrings.BtnSettingExceptionErrorMessage, ex.Message), MyStrings.Error, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void InitializeFormSize()
+        {
+            this.Width = 700;
+            this.Height = 550;
+            this.Left = (Screen.PrimaryScreen.Bounds.Width - this.Width) / 2;
+            this.Top = (Screen.PrimaryScreen.Bounds.Height - this.Height) / 2;
+        }
+
+        private void WeatherData_Load(object sender, EventArgs e)
+        {
+            DisplaySettingForm();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
